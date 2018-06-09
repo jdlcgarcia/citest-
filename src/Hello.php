a3684553eaf3dcb74ce16d5520531b2f25f10aac
Juan de la Cruz García García <jcruzgar@gmail.com>
2018-06-09 02:39:01 +0200

    Hello world class, index and gitignore

---
+++ src/Hello.php
@@ -0,0 +1,9 @@
+<?php
+namespace jdlc\citest; 
+
+class Hello {
+	public function hi() {
+		echo "Hello World!\n";
+		return true;
+	}
+}
---
+++ src/index.php
@@ -0,0 +1,4 @@
+<?php
+
+$hello = new Hello();
+$hello->greet();

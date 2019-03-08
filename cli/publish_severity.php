b8437a35f0895aca71555eb82be4331251e12fb0
jdlcgarcia <jcruzgar@gmail.com>
2019-03-09 00:45:41 +0100

    scripts for routers

---
+++ cli/publish_severity.php
@@ -0,0 +1,15 @@
+<?php
+
+use jdlc\citest\rabbitmq\ExchangeDirect;
+
+include_once '../bootstrap.php';
+
+$severity = isset($argv[1]) && !empty($argv[1]) ? $argv[1] : 'info';
+
+$data = implode(' ', array_slice($argv, 2));
+if (empty($data)) {
+    $data = "Hello World!";
+}
+
+$direct = new ExchangeDirect("logs_severity", [$severity]);
+$direct->publish($data);
---
+++ cli/receive_severity.php
@@ -0,0 +1,20 @@
+<?php
+
+use jdlc\citest\rabbitmq\ExchangeDirect;
+
+include_once '../bootstrap.php';
+
+$severities = array_slice($argv, 1);
+
+if (empty($severities)) {
+    file_put_contents('php://stderr', "Usage: $argv[0] [info] [warning] [error]\n");
+    exit(1);
+}
+
+$direct = new ExchangeDirect("logs_severity", $severities);
+
+try {
+    $direct->receive();
+} catch (ErrorException $e) {
+    echo $e->getMessage();
+}

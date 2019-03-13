dad5841817bb4846de515b1963654a840e381d72
jdlcgarcia <jcruzgar@gmail.com>
2019-03-14 00:28:59 +0100

    "bug" fixed

--- tests/FibonacciTest.php
+++ tests/FibonacciTest.php
@@ -8,7 +8,6 @@ class FibonacciTestTest extends TestCase
 
     public function testBaseCase0()
     {
-        $this->assertTrue(false);
         $fibonacho = new Fibonacci();
         $this->assertEquals(0, $fibonacho->getN(0));
     }

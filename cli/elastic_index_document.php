8df4786edaf339944eddb3bd1eee5f00a3d86a16
jdlcgarcia <jcruzgar@gmail.com>
2019-03-17 12:03:30 +0100

    index as a string (for testing)

--- cli/elastic_index_document.php
+++ cli/elastic_index_document.php
@@ -7,7 +7,7 @@ use jdlc\citest\model\Definition;
 
 
 $client = ClientBuilder::create()->build();
-$document = new Document("old_database", "old_table", "1");
+$document = new Document("old_database", "old_table", "I come in peace");
 $definition = new Definition("I come in peace", [
         Definition::ESPANOL => "Vengo en son de paz",
         Definition::KLINGON => "qo' vIvan!"
